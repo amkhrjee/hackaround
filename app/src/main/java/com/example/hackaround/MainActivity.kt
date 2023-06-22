551024160779c154b1e59fc6e7d832f7589cb8a3
amkhrjee <aniruddha.mukherji@outlook.com>
2023-06-23 02:13:17 +0530

    uploading at once

---
+++ app/src/main/java/com/example/hackaround/MainActivity.kt
@@ -0,0 +1,373 @@
+package com.example.hackaround
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.example.hackaround.ui.theme.HackAroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HackAroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background) {
+//                    GreetingImage(message = stringResource(R.string.happy_birthday_test), from = stringResource(
+//                        R.string.from_test2
+//                    ) )
+//                    DisplayArticle()
+//                    TaskCompleted()
+//                    FourQuadrant()
+                BuisinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            fontSize = 90.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun DisplayArticle(modifier: Modifier = Modifier ){
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+
+        Text(
+            text = stringResource(R.string.heading_1),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.subtitle),
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = stringResource(R.string.content),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun TaskCompleted(modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = "All tasks completed!",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp
+            )
+        )
+        Text(text = "Nice work!", fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun FourQuadrant(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        Column (
+            Modifier
+                .fillMaxHeight()
+                .weight(1f)){
+           EachQuadrant(
+               heading = "Text composable",
+               content = "Displays text and follows the recommended Material Design guidelines.",
+               color = Color(0xFFEADDFF),
+               modifier = Modifier.weight(1f),
+               maxHeight = 0.5f
+           )
+            EachQuadrant(
+                heading = "Image composable",
+                content = "Creates a composable that lays out and draws a given Painter class object.",
+                color = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f),
+                maxHeight = 1f
+            )
+        }
+        Column (
+            Modifier
+                .fillMaxHeight()
+                .weight(1f)) {
+            EachQuadrant(
+                heading = "Row composable",
+                content = "A layout composable that places its children in a horizontal sequence.",
+                color = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f),
+                maxHeight = 0.5f
+            )
+            EachQuadrant(
+                heading = "Column composable",
+                content = "A layout composable that places its children in a vertical sequence.",
+                color = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f),
+                maxHeight = 1f
+            )
+        }
+    }
+}
+
+@Composable
+fun EachQuadrant(heading: String, content: String, color: Color, maxHeight: Float, modifier:Modifier = Modifier){
+Column(
+    modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(maxHeight)
+        .background(color = color)
+        .padding(16.dp),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+) {
+    Text(
+        text = heading,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(bottom = 16.dp),
+    )
+    Text(
+        text = content,
+        textAlign = TextAlign.Justify
+    )
+    }
+}
+
+@Composable
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun BuisinessCard(modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.potrait)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize().background(color = Color.Green.copy(alpha = 0.1f)
+        )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(32.dp)
+                .weight(2f)
+        ) {
+            Image(
+                painter = image,
+                contentDescription = "Photo of Aniruddha",
+                modifier = Modifier
+                    .height(160.dp)
+                    .clip(shape = RoundedCornerShape(25.dp))
+            )
+            Text(
+                text = "Aniruddha Mukherjee",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(top = 16.dp),
+            )
+            Text(
+                text = "Professional Chole Bhature Enjoyer",
+                color = Color.DarkGray
+            )
+        }
+        Column(Modifier.padding(bottom = 24.dp)) {
+            Row(Modifier.padding(bottom = 8.dp)) {
+                Icon(
+                    Icons.Rounded.Call,
+                    contentDescription = "Phone Icon",
+                )
+                Text(
+                    text = "+91 8240625004",
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+            Row(Modifier.padding(bottom = 8.dp)) {
+                Icon(Icons.Rounded.Share, contentDescription = "Share Icon")
+                Text(
+                    text = "@amkhrjee",
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+            Row(Modifier.padding(bottom = 8.dp)) {
+                Icon(Icons.Rounded.Email, contentDescription = "Email Icon")
+                Text(
+                    text = "aniruddha.mukherji@outlook.com",
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirthDayCardPreview() {
+    HackAroundTheme {
+//        GreetingImage(message = stringResource(R.string.happy_birthday_test), from = stringResource(
+//                    R.string.from_test2) )
+//        DisplayArticle()
+//            TaskCompleted()
+//            FourQuadrant()
+//    ComposeQuadrantApp()
+        BuisinessCard()
+    }
+
+
+}
